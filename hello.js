625360a79b1f5451fc5c21a0945f7dd536076065
timconceivable <headskape@yahoo.com>
2023-09-11 14:20:22 -0500

    playing around with console and variables for quiz

--- hello.js
+++ hello.js
@@ -5,6 +5,41 @@ to the console (the panel on the right)
 */
 
 console.log('"Hello World!"');
+console.log('Hello, JavaScript.');
+console.log(2001);
+console.log("What","do","commas","do?");
+console.log("Does", "adding",      "space", "matter?");
+console.log('Launch' + 'Code');
+console.log("LaunchCode was founded in", 2013);
+console.log(typeof "17");
+console.log(typeof Number("2345"));
+console.log(Number("three"));
+console.log(Number('3 3'));
+console.log(Number('33'));
+console.log(Number('3'));
+
+
+let day = "Thursday";
+day = 32.5;
+day = 19;
+console.log(day);
+
+let a_number = 2 ** 2 ** 3 * 3;
+console.log(a_number);
+
+console.log(1 + 5 % 3);
+
+console.log(16 - 2 * 5 / 3 + 1);
+
+
+const input = require('readline-sync');
+
+let info = input.question("Please enter your age: ");
+//The user enters 25.
+
+console.log(typeof info);
+
+
 
 /*  
 Next, PLAY! 
