86e3b6032ff353c04e0030a7fd1d3885f3664b6b
timconceivable <headskape@yahoo.com>
2023-10-28 19:48:17 -0500

    added user prompt for number of words

--- spookywords.js
+++ spookywords.js
@@ -1,12 +1,12 @@
+const input = require('readline-sync');
+
 let wordList = ["cat", "bat", "tentacles", "ghost", "pumpkin", "cupcake", "spooky knife", "ghoul", "chainsaw", "rat", "zombie", "witch", "skull", "slenderman", "coffin", "vampire", "tax collector", "undead pirate", "jack-o-lantern", "vulture", "alien", "blob", "werewolf", "Dave"]
-// console.log(wordList.length);
-// console.log(index);
 
-let groupNum = 8;
+let groupNum = input.question(`how many spooky words do you want? (max ${wordList.length}):  `);
+
 let index;
-
 for (i=0; i < groupNum; i++) {
     index = Math.floor(Math.random()*wordList.length);
-    console.log(wordList.length, wordList[index]);
+    console.log(wordList[index]);
     wordList.splice(index, 1);
 }
